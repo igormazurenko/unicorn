c28967ac926aad660e84a625a19c6303a772b482
igormazurenko <igormazurenko@gmail.com>
2019-01-24 20:14:03 +0200

    rerformed some cleanup

--- Unicornsoft.ino
+++ Unicornsoft.ino
@@ -55,12 +55,12 @@ ESP8266HTTPUpdateServer httpUpdateServer;
 #define DATA_PIN      2
 #define LED_TYPE      WS2811
 #define COLOR_ORDER   GRB
-#define NUM_LEDS      60
+#define NUM_LEDS      8
 
 #define MILLI_AMPS         2000 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define FRAMES_PER_SECOND  120  // here you can control the speed. With the Access Point / Web Server the animations run a bit slower.
 
-const bool apMode = true;
+const bool apMode = false;
 
 #include "Secrets.h" // this file is intentionally not included in the sketch, so nobody accidentally commits their secret information.
 // create a Secrets.h file with the following:
